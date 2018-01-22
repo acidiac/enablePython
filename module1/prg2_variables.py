04ae3ee870f0077dafb19b2cf9896b8f8dc50ce1
amit chanchal <amit.chanchal@hotmail.com>
2018-01-23 00:09:03 +0530

    variables

---
+++ module1/prg2_variables.py
@@ -0,0 +1,13 @@
+# Variables ============================================================
+# The variables are a convenient naming mechanism for our data. 
+# In essence variable is a name that refers to a value.
+# =====================================================================
+
+
+
+
+# Reserved Words ======================================================
+# False class finally is return None continue for lambda try True def
+# from nonlocal while and del global not with as elif if or yield
+# assert else import pass break except in raise
+# =====================================================================
