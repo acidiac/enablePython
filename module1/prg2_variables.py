00d3b51429e4e8a5ce08e0723baafdb1c65e1994
amit chanchal <amit.chanchal@hotmail.com>
2018-01-23 00:22:42 +0530

    variables

--- module1/prg2_variables.py
+++ module1/prg2_variables.py
@@ -1,9 +1,33 @@
-# Variables ============================================================
+# Variables ===========================================================
 # The variables are a convenient naming mechanism for our data. 
 # In essence variable is a name that refers to a value.
 # =====================================================================
 
+# Assigning Values to Variables =======================================
+# we can assign values to a name(variable) by using "=" operator. 
+# <variable_name> = value 
+# Variable name is on the left hand and value on the right
+# Once assigned the values can be used by refering to the variable name
+# =====================================================================
 
+
+my_name = "Amit"
+# we can use print() to print it out
+print(my_name)
+
+# How do we pick a varibale name ======================================
+# As we all know picking a name can be tricky, there are few things we
+# must keep in mind when picking a variable name.
+# 1. You must avoid ambiguity.It must be explicit
+# 2. You cannot use a number in the beginning of variable name
+# 3. You cannot use reserved keywords 
+# 4. Use a style that you prefer, camel case or underscores
+# 5. Be consistent
+# =====================================================================
+
+# let's say we are trying write a program for generating marksheets
+mark_1 = 80 # this is ambiguous, what is mark_1
+marksMaths = 80 # this is better, we know what we are referring to
 
 
 # Reserved Words ======================================================
